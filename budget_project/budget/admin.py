0feda2815d6de4197b7fb025ce9082bc95fd507c
Анастасия <nastyagomorinalove@gmail.com>
2025-04-26 21:18:53 +0300

    Добавлен суперпользователь. Настроено отображение моделей на странице администратора.

--- budget_project/budget/admin.py
+++ budget_project/budget/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Income, Expense
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'category', 'date')
+    list_filter = ('category', 'date')
+    search_fields = ('user__username', 'category__name')
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'category', 'date')
+    list_filter = ('category', 'date')
+    search_fields = ('user__username', 'category__name')
